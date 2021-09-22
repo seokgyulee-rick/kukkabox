1b8680aba269d19e06581b613bd10f0784360f3f
Seokgyu Lee <dltjrrb0729@naver.com>
2021-09-23 03:56:53 +0900

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+      <li>
+        <a href="/tv">TV</a>
+      </li>
+      <li>
+        <a href="/Search">Search</a>
+      </li>
+    </ul>
+  </header>
+);
