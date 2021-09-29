89d2fc77c4771f09255ee48c3bd385552da199eb
Seokgyu Lee <dltjrrb0729@naver.com>
2021-09-29 19:19:47 +0900

    container done

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,16 +1,54 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { moviesApi, tvApi } from "api";
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+      location: { pathname },
+    } = this.props;
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+    let result;
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(id));
+      }
+      console.log(result);
+    } catch {
+      this.setState({ error: "the url is not correct" });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
 
   render() {
     const { result, error, loading } = this.state;
+    console.log(this.state);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
